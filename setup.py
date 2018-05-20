357ebc314a3c36138ccb164f117571772cf98f2e
swprojects <swprojects@gmx.com>
2018-05-20 22:28:11 +0100

    Updated flake8 testing
    Now includes parameters from config file

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ class Build(build):
         if is_root():
             fail('Running with root privileges. Try without sudo?')
 
-        self.run_command('flake8')
+        subprocess.call(['flake8', '--append-config=.flake8.ini'])
 
 
 class Install(install):
@@ -139,9 +139,8 @@ class Install(install):
         # Build the wheel
         cmd_build = [PY_EXE, '-m', 'pip', 'wheel', '-v', WX_TAR]
         subprocess.call(cmd_build)
-        
+
         # Install
-        
 
 
 class TestCommand(Command):
@@ -154,7 +153,7 @@ class TestCommand(Command):
         return
 
     def run(self):
-        self.run_command('flake8')
+        subprocess.call(['flake8', '--append-config=.flake8.ini'])
 
 
 def run_setup():
@@ -171,7 +170,7 @@ def run_setup():
           author=__author__,
           url=__homepage__,
           license='MIT',
-          packages=find_packages(),
+          packages=find_packages(exclude=['tests*']),
           install_requires=[],
           classifiers=__classifiers__,
           test_suite='tests',
